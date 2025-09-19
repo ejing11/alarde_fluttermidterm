51f46d82be614e80066380c92e412818093bb06a
ashura <mjalarde9@gmail.com>
2025-09-19 09:51:43 +0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class Homepage extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(16),
                 child: Image.network(
-                  'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80',
+                  'assets/pics/sampling.jpeg',
                   width: 200,
                   height: 120,
                   fit: BoxFit.cover,
